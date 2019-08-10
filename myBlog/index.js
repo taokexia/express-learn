78e138430be851c94c67084572d9fcf0caf53c8a
taokexia <694198149@qq.com>
2019-08-10 15:27:07 +0800

    add log 404 page

--- myBlog/index.js
+++ myBlog/index.js
@@ -6,7 +6,8 @@ const flash = require('connect-flash')
 const config = require('config-lite')(__dirname)
 const routes = require('./routes')
 const pkg = require('./package')
-
+const winston = require('winston')
+const expressWinston = require('express-winston')
 const app = express()
 
 app.set('views', path.join(__dirname, 'views')) // 设置存放模板文件的目录
@@ -46,8 +47,39 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error').toString()
   next()
 })
+  
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })
+  ]
+}))  
 // 路由
 routes(app)
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}))
+
+app.use(function (err, req, res, next) {
+  console.error(err)
+  req.flash('error', err.message)
+  res.redirect('/posts')
+})
 
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
